2d3b91b7ac520b79292466b1ef185bae1b3414c9
anacpbarros <barros.anacp@gmail.com>
2021-01-10 23:36:30 -0300

    definir fim do jogo

--- main.js
+++ main.js
@@ -44,6 +44,13 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (i = 1; i <snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG(); // criar background
     criarCobrinha();
     comidaCobrinha();
@@ -64,14 +71,12 @@ function iniciarJogo() {
         snakeY += box;
     }
 
-    if(snakeX != comidaCobrinha.x || snakeY != comidaCobrinha.y){
+    if(snakeX != comida.x || snakeY != comida.y){
         snake.pop();
     } else {
-        comidaCobrinha.x = Math.floor(Math.random() * 15 + 1) * box;
-        comidaCobrinha.y = Math.floor(Math.random() * 15 + 1) * box;
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-    
 
     let novaCabeca = {
         x: snakeX,
