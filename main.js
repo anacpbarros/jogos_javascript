e3f0f83a71a3e8b57ab5afc9d151810adf53e3b2
anacpbarros <barros.anacp@gmail.com>
2021-01-10 22:41:46 -0300

    definir movimentos da cobrinha e ajuste do src do <script>

--- main.js
+++ main.js
@@ -6,18 +6,49 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightpurple";
     context.fillRect(0, 0, 16 * box, 16 * box); // (x, y, heigth, width)
 }
 
 function criarCobrinha(){
-    for (i = 0; i < snake.lenght; i++){
-        context.fillStyle = "green";
+    for (i = 0; i < snake.length; i++){
+        context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG(); // criar background
-criarCobrinha();
+function iniciarJogo() {
+    criarBG(); // criar background
+    criarCobrinha();
+
+    let snakeX = snake[0].x; //começa na posição 0 de x e de y
+    let snakeY = snake[0].y;
+
+    if(direction == "right") {
+        snakeX += box;
+    }
+    if(direction == "left") {
+        snakeX -= box;
+    }
+    if(direction == "up") {
+        snakeY -= box;
+    }
+    if(direction == "down") {
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //entra no lugar do primero elemento
+}
+
+let jogo = setInterval(iniciarJogo, 100); //a cada 100 milisegundos renova o jogo caso travar
+
