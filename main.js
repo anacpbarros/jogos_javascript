f9a173a3ab08ec403f945eb4c49d3e6c2ec33be2
anacpbarros <barros.anacp@gmail.com>
2021-01-10 23:21:42 -0300

    definir aleatoriedade da frutinha e tamanho da cobrinha

--- main.js
+++ main.js
@@ -7,31 +7,46 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box, //metodo floor tira o ponto flutuante
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightpurple";
+    context.fillStyle = "purple";
     context.fillRect(0, 0, 16 * box, 16 * box); // (x, y, heigth, width)
 }
 
 function criarCobrinha(){
     for (i = 0; i < snake.length; i++){
-        context.fillStyle = "purple";
+        context.fillStyle = "gray";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function comidaCobrinha(){
+    context.fillStyle = "red"
+    context.fillRect(comida.x, comida.y, box, box);
 }
 
 document.addEventListener('keydown', update); //keydown recebe comando do teclado e vai chamar a função update
 
 function update(event){
-    if(event.keyCode == 37 && direction != "right") directon = "left"; //pode-se escrever o if simples assim
-    if(event.keyCode == 38 && direction != "down") directon = "up";
-    if(event.keyCode == 39 && direction != "left") directon = "right";
-    if(event.keyCode == 40 && direction != "up") directon = "down";
+    if(event.keyCode == 37 && direction != "right") direction = "left"; //pode-se escrever o if simples assim
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG(); // criar background
     criarCobrinha();
+    comidaCobrinha();
 
     let snakeX = snake[0].x; //começa na posição 0 de x e de y
     let snakeY = snake[0].y;
@@ -49,14 +64,21 @@ function iniciarJogo() {
         snakeY += box;
     }
 
-    snake.pop();
+    if(snakeX != comidaCobrinha.x || snakeY != comidaCobrinha.y){
+        snake.pop();
+    } else {
+        comidaCobrinha.x = Math.floor(Math.random() * 15 + 1) * box;
+        comidaCobrinha.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
-    let newHead = {
+    
+
+    let novaCabeca = {
         x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(newHead); //entra no lugar do primero elemento
+    snake.unshift(novaCabeca); //entra no lugar do primero elemento
 }
 
 let jogo = setInterval(iniciarJogo, 100); //a cada 100 milisegundos renova o jogo caso travar
